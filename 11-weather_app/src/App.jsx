dd145697b67d2e90e6aeb3fcea0c54b89e4e61cc
Jesús Isidro Regules Santiago <isc20350312@gmail.com>
2024-05-09 11:15:53 -0600

    Weather App

--- 11-weather_app/src/App.jsx
+++ 11-weather_app/src/App.jsx
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react"
-import { getWeather } from "./api/WeatherApi"
-import CityCard from './components/CityCard'
+
+import CityCard from './components/CityCard/index'
 
 
 const App = () => {
-  const [weather, setWeather] = useState(null)
-
-  useEffect(() => {
-      const getCityWeather = async () => {
-    setWeather(await getWeather())
-  }
-
-  getCityWeather()
-
-  }, [])
 
 
   return (
@@ -21,8 +10,8 @@ const App = () => {
       <h1>Weather Api</h1>
       <hr />
       {
-        !weather ? <h2>Loading...</h2> :
-        <CityCard weather={weather}/>
+        
+        <CityCard />
       }
       
       </>
--- 11-weather_app/src/components/CityCard/CityCardContent.jsx
+++ 11-weather_app/src/components/CityCard/CityCardContent.jsx
@@ -1,7 +1,7 @@
-const CityCard  = ({weather}) => {
+const CityCardContent = ({weather}) => {
     return(
-        <div className="card" style={{width: '18rem'}}>
-            <img 
+        <>
+         <img 
             src={`http:${weather.current.condition.icon}`} 
             className="card-img-top" 
             alt="weather.current.condition.text"/>
@@ -9,8 +9,9 @@ const CityCard  = ({weather}) => {
             <div className="card-body">
                 <h5 className="card-title">{weather.location.name}</h5>
                 <p className="card-text">{weather.current.condition.text}</p>
-                </div>  
-        </div>
+                </div> 
+        </>
     )
 }
-export default CityCard
+
+export default CityCardContent
